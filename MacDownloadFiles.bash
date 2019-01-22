fc451a5385d4796c99229e4f45855dd4b7f521bd
Phill <14913130+phillram@users.noreply.github.com>
2019-01-22 15:00:44 -0800

    Created Mac bash for execution

---
+++ MacDownloadFiles.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o /tmp/puppy.jpg https://raw.githubusercontent.com/phillram/saucelabs-import-files/master/puppy.jpg
+
+curl -o /tmp/taco.jpg https://raw.githubusercontent.com/phillram/saucelabs-import-files/master/taco.jpg
