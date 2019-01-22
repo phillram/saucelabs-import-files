1f8ff13e807586731afb3320e3ac6a8a2449a750
Phill <14913130+phillram@users.noreply.github.com>
2019-01-22 15:02:41 -0800

    Created Linux bash for execution

---
+++ LinuxDownloadFiles.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O /tmp/puppy.txt https://raw.githubusercontent.com/phillsauce/saucelabs-import-files/master/puppy.jpg
+
+wget -O /tmp/taco.txt https://raw.githubusercontent.com/phillsauce/saucelabs-import-files/master/taco.jpg
