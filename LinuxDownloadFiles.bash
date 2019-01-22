7e7cb38968ddcb2238d8aced32aac862936a0073
Phill <14913130+phillram@users.noreply.github.com>
2019-01-22 15:03:22 -0800

    Updated URL to point to right repository

--- LinuxDownloadFiles.bash
+++ LinuxDownloadFiles.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O /tmp/puppy.txt https://raw.githubusercontent.com/phillsauce/saucelabs-import-files/master/puppy.jpg
+wget -O /tmp/puppy.txt https://raw.githubusercontent.com/phillram/saucelabs-import-files/master/puppy.jpg
 
-wget -O /tmp/taco.txt https://raw.githubusercontent.com/phillsauce/saucelabs-import-files/master/taco.jpg
+wget -O /tmp/taco.txt https://raw.githubusercontent.com/phillram/saucelabs-import-files/master/taco.jpg
