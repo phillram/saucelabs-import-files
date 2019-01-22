a49ba72a03f354e5787ab2ab652af5ef9e61cd72
Phill <14913130+phillram@users.noreply.github.com>
2019-01-22 15:01:56 -0800

    Created Windows batch for execution

---
+++ WinDownloadFiles.bat
@@ -0,0 +1,4 @@
+@echo off
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/phillram/saucelabs-import-files/master/puppy.jpg C:\Users\Administrator\Desktop\puppy.jpg
+
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/phillram/saucelabs-import-files/master/taco.jpg C:\Users\Administrator\Desktop\taco.jpg
